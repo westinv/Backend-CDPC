0a9b9ffb25c939bfb32d734b03ae11bb652402ce
Victor Westim <westin_2014v@hotmail.com>
2020-08-22 14:06:56 -0300

    Commit com o CRUD

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const users = ["victor", "Waislan"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+  return res.sand();
+});
+server.listen(3000);
